d66983790c2ea56577a36cb05d701d2395938190
Lujza Milotova <milotovl@natur.cuni.cz>
2024-04-17 11:42:36 +0200

    Update visualization

--- code/7_table6_vis_prep.py
+++ code/7_table6_vis_prep.py
@@ -2,6 +2,32 @@ import pandas as pd
 import json
 import math
 
+def align(dna_sequence, substring_size, repeats):
+    result = []
+    i = 0
+    counter = 0
+    while i < len(dna_sequence):
+        if i + substring_size > len(dna_sequence):
+            result.append(list(dna_sequence[i:]))
+            break
+        current_substring = dna_sequence[i:i+substring_size]
+        if current_substring in repeats and counter == 0: 
+            result.extend(list(dna_sequence[i:i+substring_size]))
+            i += substring_size
+        else:
+            if counter > 0 and current_substring in repeats:
+                fill_size = substring_size - counter
+                result.extend([''] * fill_size)
+                counter = 0
+                continue
+            result.append(dna_sequence[i])
+            counter = (counter + 1) % substring_size
+            i += 1
+    if counter != 0 and counter < substring_size:
+        result.extend([''] * (substring_size - counter))
+
+    return result
+
 json_file_path = 'data/transformed_data.json'
 
 with open(json_file_path, 'r') as file:
@@ -20,6 +46,7 @@ for marker, marker_info in sorted(data['markers'].items(), key=lambda x: x[1].ge
 
     reference_allele = marker_info.get('referenceAllele', {})
     chromosome = marker_info.get('chromosome', 0)
+    start_coordinate = marker_info.get('startCoordinate', 0)
 
     ref_num_of_repeats = reference_allele.get('numberOfRepeats', 0)
     ref_sequence = list(reference_allele.get('sequence', ''))
@@ -27,32 +54,62 @@ for marker, marker_info in sorted(data['markers'].items(), key=lambda x: x[1].ge
     ref_after = list(reference_allele.get('after', ''))
 
     before_length = len(ref_before)
+    seq_length = len(ref_sequence)
     after_length = len(ref_after)
     
+    # rozsirenenie ref. sekvencie, ak treba
     max_number_of_repeats = 0 # na zistenie o kolko treba rozsirit ref. sekvenciu
     for allele_var in marker_info['lengthVariants']:
         if allele_var["numberOfRepeats"] > max_number_of_repeats:
             max_number_of_repeats = allele_var["numberOfRepeats"]
-    
-    if max_number_of_repeats > ref_num_of_repeats: # ak je ref. sekv. prikratka, vlozime prazdne miesta
-        ref_sequence = ref_sequence + [''] * ((math.ceil(max_number_of_repeats) - ref_num_of_repeats) * str_length)
+    fillers = (math.ceil(max_number_of_repeats) - ref_num_of_repeats) * str_length
+    if max_number_of_repeats > ref_num_of_repeats: # ak je ref. sekv. prikratka, rozsirime ju = vlozime prazdne miesta
+        ref_sequence = ref_sequence + [''] * fillers
 
+    print(ref_num_of_repeats)
+    
+    # cislovanie repeats v ref. sekvencii
     numbering = ['' for _ in range(before_length)]
     for num in range(1, ref_num_of_repeats + 1):
         numbering.append(num)
         for i in range(str_length - 1):
             numbering.append('')
+
+    # GRCh38 suradnice jednotlivych nukleotidov v ref. sekvencii
+    coordinates = ['', '', 'GRCh38 coordinates']
+    for num in range(start_coordinate - before_length, start_coordinate + seq_length):
+        coordinates.append(num)
+    coordinates += [''] * fillers
+    for num in range(start_coordinate + seq_length, start_coordinate + seq_length + after_length):
+        coordinates.append(num)
+
+    # cislovanie vzdialenosti od repeat region
+    distance = ['', '', 'Distance from repeat region']
+    for i in range(0, before_length):
+        distance.append(before_length - i)
+    distance += [''] * fillers + [''] * seq_length
+    for i in range(1, after_length + 1):
+        distance.append(i)
+
+    # append uvodnych riadkov
     rows.append([marker, 'Allele', 'Chr' + str(chromosome)] + numbering)
-    rows.append(['', '', ''] + ref_before + ref_sequence + ref_after)
-
-
+    rows.append(['', '', 'Reference sequence'] + ref_before + ref_sequence + ref_after)
+    rows.append(coordinates)
+    rows.append(distance)
 
     # iterovanie cez vsetky varianty alel ZORADENE podla 'allele'
     for allele_var in sorted(marker_info['lengthVariants'], key=lambda x: x['numberOfRepeats']): 
         allele = allele_var['numberOfRepeats']
+        if allele == 0: continue
+
         for seq_var in allele_var['sequenceVariants']:
             sequence = seq_var['sequence']
-            if seq_var['flankingRegionsVariants']: # ak existuju flanking region varianty
+
+            # TODO align sequence - skusit podla ref. sekv.
+            if allele % 1 != 0:
+                sequence = align(sequence, str_length, repeats)
+
+            if seq_var['flankingRegionsVariants']: # ak existuju flanking region varianty, treba ich pripojit do riadku
                 for flank_var in seq_var['flankingRegionsVariants']:
                     before = flank_var['before']
                     after = flank_var['after']
@@ -73,12 +130,10 @@ for marker, marker_info in sorted(data['markers'].items(), key=lambda x: x[1].ge
                     
                     rows.append(['', allele, ''] + row)
 
-            else: # ak neexistuju
+            else: # ak neexistuju, tak namiesto nich prazdne miesta
                 row = ['' for _ in range(before_length)] + list(sequence)
                 rows.append(['', allele, ''] + row)
-    rows.append([]) # prazdne riadky na oddelenie
-    rows.append([])
-    rows.append([])
+    rows.append([]) # prazdny riadkok na oddelenie od nasledujuceho markera
 
 df = pd.DataFrame(rows)
 
